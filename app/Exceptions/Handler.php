e5da6bd545384d2d539d0faaf219c2307cc96b0e
mookemjung <artzpluz@gmail.com>
2018-05-10 16:39:18 +0700

    Handle Exceptions ModelNotFoundException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException as ModelNotFoundException;
 
 class Handler extends ExceptionHandler
 {
@@ -48,6 +49,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        //dd($exception);
+        if($exception instanceof ModelNotFoundException) {
+            return response()->json('Model not found', 404);
+            //return 'model not found';
+        }
+
         return parent::render($request, $exception);
     }
 }
